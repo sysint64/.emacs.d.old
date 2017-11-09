12febd00e3c2d332ef6e942fa3d75d8fa6f637d6
Andrey Kabylin <andrey@kabylin.ru>
2017-11-09 23:11:23 +0700

    clean init.el

--- init.el
+++ init.el
@@ -14,21 +14,5 @@
 (eval-when-compile
   (require 'use-package))
 
-;; (add-to-list 'load-path "~/.emacs.d")
 (load-file "~/.emacs.d/loader.el")
 (load-layers)
-
-;; (require 'loader)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (neotree use-package magit))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
