6ed7712b6d05bd10743251f7d23cc8a9b3273b4c
Andrey Kabylin <andrey@kabylin.ru>
2021-03-24 06:59:56 +0000

    custus-set-variables

--- init.sample.el
+++ init.sample.el
@@ -19,3 +19,11 @@
 (set-face-font 'default "Hack-12")
 (require 'theme-dark)
 (setq inhibit-startup-screen t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(grep-find-template "find . <X> -type f <F> -exec grep <C> -nHI -e <R> {} +")
+ '(helm-projectile-grep-command "grep -a -r %e -n%cHI -e %p %f ."))
